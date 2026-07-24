f3728798ef158ea2ad2cf6ee332ff8f50a07fd3c
baizhu945 <xuel3087@gmail.com>
2026-07-24 23:15:35 +0800

    Fixed Ollama pkgs BUG.

--- nixos/llm-cuda.nix
+++ nixos/llm-cuda.nix
@@ -4,10 +4,38 @@ let
   unstableTarball =
     fetchTarball
       "https://nixos.org/channels/nixpkgs-unstable/nixexprs.tar.xz";
-  unstablePkgs = import unstableTarball {
-    config = config.nixpkgs.config;
+
+  overlay = final: prev: {
+    ollama-cuda = prev.ollama-cuda.overrideAttrs (oldAttrs:
+      let
+        cudaLibs = with prev.cudaPackages; [ cuda_cudart libcublas cccl ];
+        cudaMajorVersion = prev.lib.versions.major prev.cudaPackages.cuda_cudart.version;
+        cudaToolkit = prev.buildEnv {
+          name = "cuda-merged-${cudaMajorVersion}";
+          paths = map prev.lib.getLib cudaLibs ++ [
+            (prev.lib.getOutput "static" prev.cudaPackages.cuda_cudart)
+            (prev.lib.getBin (prev.cudaPackages.cuda_nvcc.__spliced.buildHost or prev.cudaPackages.cuda_nvcc))
+          ];
+          ignoreCollisions = true;
+        };
+      in {
+        preBuild = ''
+          export CUDAToolkit_ROOT="${cudaToolkit}"
+          for i in "''${!cmakeFlagsArray[@]}"; do
+            if [[ ''${cmakeFlagsArray[i]} == -DCUDAToolkit_ROOT=* ]]; then
+              cmakeFlagsArray[i]="-DCUDAToolkit_ROOT=${cudaToolkit}"
+            fi
+          done
+          cmakeFlags=''${cmakeFlagsArray[*]}
+        '' + (oldAttrs.preBuild or "");
+      }
+    );
   };
 
+  unstablePkgs = import unstableTarball {
+    config = config.nixpkgs.config // { allowUnfree = true; };
+    overlays = [ overlay ];
+  };
 in
 {
   services.ollama = {
